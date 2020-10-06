91fcd1a77d7b860728b65947aca3ef380cd3a0b4
Tamas K. Papp <tkpapp@gmail.com>
2020-10-06 09:15:01 +0200

    factor out test problems to NonLinearTestProblems

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
-using LinearAlgebra, Test, TrustRegionMethods, UnPack, Logging
+using LinearAlgebra, Test, TrustRegionMethods, UnPack, Logging, NonlinearTestProblems
 import Optim
 
 # log debug messages
 global_logger(SimpleLogger(stdout, Logging.Debug))
 
 include("test_building_blocks.jl")
-include("test_simple_problems.jl")
+include("test_problems.jl")
 include("test_nonlinear_problems.jl")
